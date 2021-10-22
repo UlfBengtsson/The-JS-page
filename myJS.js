4e0afefc9eb2d7453f4b2af7dd47b92c47007865
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-10-22 09:50:07 +0200

    no tictactoe boeard yet

--- myJS.js
+++ myJS.js
@@ -1,20 +1,44 @@
 "use strict";
 
 document.getElementsByTagName("body")[0].style.backgroundColor = "lightgoldenrodyellow";
-
+var boxContainer = document.getElementById("boxContainer");
 var navLinks = document.getElementsByClassName("navLink");
 
 for (let index = 0; index < navLinks.length; index++) {
     navLinks[index].innerHTML = "Link " + (index + 1);
 }
 
-function makeBox() {
+document.getElementById("tictactoe").addEventListener("click", () => createTicTacToe());
+
+
+function makeBox(boxColor) {
     console.log("makeBox stated");
 
     let myBox = document.createElement("div");
     myBox.className = "box";
-    document.getElementById("boxContainer").appendChild(myBox);
+    myBox.classList.add(boxColor);
+
+    boxContainer.appendChild(myBox);
     console.log("Box added", myBox);
+}
+
+function createTicTacToe() {
+
+    boxContainer.style.width = '150px';
+
+    let toggleColor = false;
+
+    for (let index = 0; index < 9; index++) {
+
+        if (toggleColor) {
+            makeBox("boxWhite");
+        }
+        else {
+            makeBox("boxBlack");
+        }
+        toggleColor = !toggleColor;
+    }
+
 }
 
 //makeBox();
